098e932d9fbf064d1a88089d117c8e96f9b4cca7
cvmug <catherine.mugnai@gmail.com>
2023-02-21 21:56:39 -0500

    described products db functions

---
+++ db/products.js
@@ -0,0 +1,13 @@
+// getProducts(): Retrieves a list of all products in the database.
+
+// getProductById(id): Retrieves a single product by its ID.
+
+// getProductsByCategory(category): Retrieves a list of products that belong to a particular category.
+
+// getProductsBySubcategory(subcategory): Retrieves a list of products that belong to a particular subcategory.
+
+// createProduct(productData): Creates a new product with the given data.
+
+// updateProduct(id, updates): Updates an existing product with new data.
+
+// deleteProduct(id): Deletes a product from the database by its ID.
