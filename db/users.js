707fce6fa279a45880508d1abdfb88665067d2a9
Meanithhuon <huon.meanith@gmail.com>
2023-02-22 14:12:04 -0600

    add db/users.js

--- db/users.js
+++ db/users.js
@@ -2,10 +2,74 @@
 const client = require('../client');
 
 async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
+  try {
+    const { rows: users } = await client.query(`
+      SELECT * FROM users;
+    `);
+    return users;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUserById(id) {
+  try {
+    const { rows: user } = await client.query(`
+      SELECT * FROM users
+      WHERE id=$1;
+    `, [id]);
+    return user[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createUser(userData) {
+  const { username, password, email, isAdmin } = userData;
+  try {
+    const { rows: [user] } = await client.query(`
+      INSERT INTO users(username, password, email, "isAdmin")
+      VALUES($1, $2, $3, $4)
+      RETURNING *;
+    `, [username, password, email, isAdmin]);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUser(id, updates) {
+  const setString = Object.keys(updates).map((key, index) => `"${key}"=$${index + 1}`).join(', ');
+  const values = Object.values(updates);
+  values.push(id);
+  try {
+    const { rows: [user] } = await client.query(`
+      UPDATE users
+      SET ${setString}
+      WHERE id=$${values.length}
+      RETURNING *;
+    `, values);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteUser(id) {
+  try {
+    await client.query(`
+      DELETE FROM users
+      WHERE id=$1;
+    `, [id]);
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
-  // add your database adapter fns here
   getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
 };
