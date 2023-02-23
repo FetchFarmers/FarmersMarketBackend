bf440f1b7a459824dab32530ddd15cfdbcf4949c
cvmug <catherine.mugnai@gmail.com>
2023-02-23 15:51:58 -0500

    changes to file paths and added app.listen

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.listen(5000, () => {
+  console.log("server has started on port 5000")
+});
+
 const client = require('./db/client');
 client.connect();
 
