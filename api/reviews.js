be9ca231a091a41cadb9a739ae399cef837704ab
Meanithhuon <huon.meanith@gmail.com>
2023-03-02 22:12:58 -0600

    add reviews

--- api/reviews.js
+++ api/reviews.js
@@ -1,28 +1,36 @@
 const express = require('express');
-const { requireUser } = require('./utils');
+const { requireUser, requireAdmin } = require('./utils');
 const reviewsRouter = express.Router();
 
 const { 
-    // add in database adapter functions
+  getAllReviewsByProductId,
+  createReview,
 } = require('../db');
 
+// Get all reviews for a product by product id
+reviewsRouter.get('/:productId/reviews', async (req, res, next) => {
+  const { productId } = req.params;
 
+  try {
+    const reviews = await getAllReviewsByProductId(productId);
+    res.send(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
 
+// Create a new review for a product
+reviewsRouter.post('/:productId/reviews', requireUser, async (req, res, next) => {
+  const { productId } = req.params;
+  const { title, details, starRating } = req.body;
+  const { user } = req;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  try {
+    const review = await createReview(productId, { title, details, starRating, userId: user.id });
+    res.send(review);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = reviewsRouter;
